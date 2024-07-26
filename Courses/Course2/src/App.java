4685aea53dbaf6a0088717e72b43c0109ed8eae9
ardenfndi <emre.cubukm@hotmail.com>
2024-07-26 11:38:29 +0300

    Update

---
+++ Courses/Course2/src/App.java
@@ -0,0 +1,153 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Define an array of integers that has 5 elements and let user input the values of the array. 
+        // Then, print the sum of the elements in the array.
+
+        // dataType[] varName;  ---- declare an array
+        // dataType[] varName = new dataType[size];  ---- declare and initialize an array
+
+        // Scanner scanner = new Scanner(System.in);
+        // int[] numbers = new int[5];
+        
+        // for(int i=0; i < numbers.length; i++) {
+        //     System.out.print("Enter a number-" + (i+1) + ": ");
+        //     numbers[i] = scanner.nextInt();
+        // }
+        
+        // int sum = 0;
+
+        // for(int i = 0; i < numbers.length; i++){
+        //     sum += numbers[i];
+        // }
+        
+        // System.out.println("Sum of the numbers are: " + sum);
+
+        // ------------------------------------------------------------------------------------------------
+
+        /*
+            Define an array of integers that has 5 elements and let user input the values of the array. 
+            Then, print the max and min values of the elements in the array.
+        */
+        
+        // Scanner scanner = new Scanner(System.in);
+        // int[] numbers = new int[5];
+
+        // System.out.print("Enter numbers: ");
+        // for(int i = 0; i<numbers.length; i++){
+        //     numbers[i] = scanner.nextInt();
+        // }
+
+        // int maxNumber = numbers[0];
+
+        // for(int i = 0; i< numbers.length; i++){
+        //     if(numbers[i] > maxNumber){
+        //         maxNumber = numbers[i];
+        //     }
+        // }
+
+        // int minNumber = numbers[0];
+
+        // for(int i = 0; i < numbers.length; i++){
+        //     if(numbers[i] < minNumber){
+        //         minNumber = numbers[i];
+        //     }
+        // }
+
+        // System.out.println("Minimum number is: " + minNumber + "\nMaximum number is: " + maxNumber);
+
+        // ------------------------------------------------------------------------------------------------
+
+        // Define an array of integers that has 5 elements and let user input the values of the array.
+        // Then, print the average of the elements in the array.
+
+        // Scanner scanner = new Scanner(System.in);
+        // int[] numbers = new int[5];
+
+        // System.out.print("Enter numbers: ");
+        // for (int i = 0; i< numbers.length; i++){
+        //     numbers[i] = scanner.nextInt();
+        // }
+
+        // int sum = 0;
+        // for(int i = 0; i< numbers.length; i++){
+        //     sum += numbers[i];
+        // }
+
+        // double avarage = (double)sum/numbers.length;
+        // System.out.println("Avarage is: " + avarage);
+
+        // ------------------------------------------------------------------------------------------------
+
+        // Define an array of integers that has 5 elements and let user input the values of the array.
+        // Then, sort the array in ascending order and print the sorted array.
+
+        // Scanner scanner = new Scanner(System.in);
+        // int[] numbers = new int[5];
+        
+        // System.out.print("Enter numbers: ");
+        
+        // for(int i = 0; i< numbers.length; i++){
+        //     numbers[i] = scanner.nextInt();
+        // }
+        
+        // // Bubble sort
+        // for(int i = 0; i< numbers.length; i++){
+        //     for(int j = 0; j < numbers.length-1; j++){
+        //         if(numbers[j] > numbers[j+1]){
+        //             int number = numbers[j];
+        //             numbers[j] = numbers[j+1];
+        //             numbers[j+1] = number;
+        //         }
+        //     }
+        // }
+       
+        // System.out.print("Numbers: ");
+        // for(int  i = 0; i< numbers.length; i++){
+        //     System.out.print(numbers[i] + " ");
+        // }
+
+        /*
+            Iter-1
+            S1 --> [5, 3, 1, 4, 2]
+            S2 --> [3, 5, 1, 4, 2]
+            S3 --> [3, 1, 5, 4, 2]
+            S4 --> [3, 1, 4, 5, 2]
+            S5 --> [3, 1, 4, 2, 5]
+
+            Iter-2
+            S1 --> [3, 1, 4, 2, 5]
+            S2 --> [1, 3, 4, 2, 5]
+            S3 --> [1, 3, 4, 2, 5]
+            S4 --> [1, 3, 2, 4, 5]
+            S5 --> [1, 3, 2, 4, 5]
+
+            Iter-3
+            S1 --> [1, 3, 2, 4, 5]
+            S2 --> [1, 3, 2, 4, 5]
+            S3 --> [1, 2, 3, 4, 5]
+            S4 --> [1, 2, 3, 4, 5]
+            S5 --> [1, 2, 3, 4, 5]
+
+            Iter-4
+            S1 --> [1, 2, 3, 4, 5]
+            S2 --> [1, 2, 3, 4, 5]
+            S3 --> [1, 2, 3, 4, 5]
+            S4 --> [1, 2, 3, 4, 5]
+            S5 --> [1, 2, 3, 4, 5]
+
+            Iter-5
+            S1 --> [1, 2, 3, 4, 5]
+            S2 --> [1, 2, 3, 4, 5]
+            S3 --> [1, 2, 3, 4, 5]
+            S4 --> [1, 2, 3, 4, 5]
+            S5 --> [1, 2, 3, 4, 5]
+        */
+
+        // ------------------------------------------------------------------------------------------------
+        
+        
+        
+    }
+}
