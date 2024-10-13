6362a6b2eb590dfc826bc6e5d94d6ae08dc40a7b
ardenfndi <emre.cubukm@hotmail.com>
2024-10-13 14:27:10 +0300

    Update

---
+++ Exercises/Exam/src/Bag/LinkedBag.java
@@ -0,0 +1,131 @@
+package Bag;
+
+public class LinkedBag<T> implements BagInterface<T> {
+    private Node firstNode;
+    private int numberOfEntries;
+
+    public LinkedBag() {
+        firstNode = null;
+        numberOfEntries = 0;
+    }
+
+    public boolean add(T newEntry) {
+        Node newNode = new Node(newEntry, firstNode);
+        firstNode = newNode;
+        numberOfEntries++;
+        return true;
+    }
+
+    public T[] toArray() {
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) new Object[numberOfEntries];
+        int index = 0;
+        Node currentNode = firstNode;
+        while ((index < numberOfEntries) && (currentNode != null)) {
+            result[index] = currentNode.data;
+            index++;
+            currentNode = currentNode.next;
+        }
+        return result;
+    }
+
+    public int getFrequencyOf(T anEntry) {
+        int frequency = 0;
+        int counter = 0;
+        Node currentNode = firstNode;
+
+        while ((counter < numberOfEntries) && (currentNode != null)) {
+            if (anEntry.equals(currentNode.data)) {
+                frequency++;
+            }
+            counter++;
+            currentNode = currentNode.next;
+        }
+
+        return frequency;
+    }
+
+    public boolean remove(T anEntry) {
+        Node nodeToRemove = getReferenceTo(anEntry);
+        if (nodeToRemove != null) {
+            nodeToRemove.data = firstNode.data;
+            firstNode = firstNode.next;
+            numberOfEntries--;
+            return true;
+        }
+        return false;
+    }
+
+    public T remove() {
+        T result = null;
+        if (firstNode != null) {
+            result = firstNode.data;
+            firstNode = firstNode.next;
+            numberOfEntries--;
+        }
+        return result;
+    }
+
+    public boolean contains(T anEntry) {
+        return getReferenceTo(anEntry) != null;
+    }
+
+    public int getCurrentSize() {
+        return numberOfEntries;
+    }
+
+    public void display() {
+        Node currentNode = firstNode;
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    }
+
+    public void clear() {
+        while (!isEmpty()) {
+            remove();
+        }
+    }
+
+    public boolean isEmpty() {
+        return numberOfEntries == 0;
+    }
+
+    public boolean isFull() {
+        return false; 
+    }
+
+    private Node getReferenceTo(T anEntry) {
+        boolean found = false;
+        Node currentNode = firstNode;
+
+        while (!found && (currentNode != null)) {
+            if (anEntry.equals(currentNode.data)) {
+                found = true;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        return currentNode;
+    }
+
+    private class Node {
+        private T data;
+        private Node next;
+
+        private Node(T dataPortion) {
+            this(dataPortion, null);
+        }
+
+        private Node(T dataPortion, Node nextNode) {
+            data = dataPortion;
+            next = nextNode;
+        }
+
+        @SuppressWarnings("unused")
+        private T getData() {
+            return data;
+        }
+    }
+}
